4c12d583e81505d0c4ae175023f6d0c84f9418a4
Jhon Livias <jhonlivias3@gmail.com>
2025-05-02 09:34:28 -0500

    product sizes

--- src/app/private/inventories/products/pages/form/sizes/sizes-form.component.ts
+++ src/app/private/inventories/products/pages/form/sizes/sizes-form.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+import { Table, TableModule } from 'primeng/table';
+
+import { ToolbarModule } from 'primeng/toolbar';
+import { SizesSelectedService } from '../../../../size/services/sizes-selected.service';
+import { Size } from '../../../models/products.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sizes-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ToolbarModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    RippleModule,
+  ],
   templateUrl: './sizes-form.component.html',
   styleUrl: './sizes-form.component.scss',
 })
-export class SizesFormComponent {}
+export class SizesFormComponent implements OnInit {
+  productId: number = 0;
+  products: any[] = [];
+  sizeTypes: Size[] = [];
+  selectedProducts: any[] = [];
+  cols: any[] = [];
+
+  constructor(
+    private readonly sizesSelectedService: SizesSelectedService,
+    private readonly route: ActivatedRoute,
+  ) {
+    if (this.route.snapshot.paramMap.get('id')) {
+      this.productId = Number(this.route.snapshot.paramMap.get('id'));
+    }
+  }
+
+  openNew() {}
+  deleteSelectedProducts() {}
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  ngOnInit(): void {
+    this.getSizes();
+    this.getSizeTypes();
+  }
+
+  getSizeTypes() {
+    this.sizesSelectedService.getSizeTypes().subscribe({
+      next: (sizeTypes: Size[]) => {
+        this.sizeTypes = sizeTypes;
+      },
+    });
+  }
+
+  async getSizes(): Promise<void> {
+    this.sizesSelectedService.callGetList(this.productId).subscribe();
+  }
+
+  get sizes(): Observable<Size[]> {
+    return this.sizesSelectedService.getList();
+  }
+}
---
+++ src/app/private/inventories/size/services/sizes-selected.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Size, SizeSave } from '../models/sizes.model';
+import {
+  BehaviorSubject,
+  debounceTime,
+  map,
+  Observable,
+  switchMap,
+} from 'rxjs';
+import { ApiService } from '../../../../services/api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SizesSelectedService {
+  sizes: Size[] = [];
+  sizes$: BehaviorSubject<Size[]> = new BehaviorSubject<Size[]>(this.sizes);
+  constructor(private apiService: ApiService) {}
+  callGetList(productId: number): Observable<void> {
+    let url = `sizes/selected`;
+    if (productId) {
+      url += `?productId=${productId}&sizeTypeId=3`;
+    }
+    return this.apiService.get<Size[]>(url).pipe(
+      debounceTime(600),
+      map((size: Size[]) => {
+        this.updateSizes(size);
+      }),
+    );
+  }
+
+  getList(): Observable<Size[]> {
+    return this.sizes$.asObservable();
+  }
+
+  create(data: SizeSave, productId: number): Observable<void> {
+    return this.apiService
+      .post('sizes', data)
+      .pipe(switchMap(() => this.callGetList(productId)));
+  }
+
+  getSizeTypes(): Observable<Size[]> {
+    return this.apiService.get('size-types');
+  }
+
+  private updateSizes(value: Size[]): void {
+    this.sizes = value;
+    this.sizes$.next(this.sizes);
+  }
+}
